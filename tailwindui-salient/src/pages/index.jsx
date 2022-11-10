5b3cad7a49c43ccee08e3cfd64f16b2ea1a88b38
maxmcnich <108849911+maxmcnich@users.noreply.github.com>
2022-11-10 11:07:50 -0800

    systemfiles

---
+++ tailwindui-salient/src/pages/index.jsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+
+import { CallToAction } from '@/components/CallToAction'
+import { Faqs } from '@/components/Faqs'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Hero } from '@/components/Hero'
+import { PrimaryFeatures } from '@/components/PrimaryFeatures'
+import { SecondaryFeatures } from '@/components/SecondaryFeatures'
+import { Testimonials } from '@/components/Testimonials'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Veza</title>
+        <meta
+          name="description"
+          content="An anonymous social platform targeted at mental health awareness."
+        />
+      </Head>
+      <Header />
+      <main>
+        <Hero />
+        <PrimaryFeatures />
+        <SecondaryFeatures />
+        <CallToAction />
+        <Testimonials />
+        <Faqs />
+      </main>
+      <Footer />
+    </>
+  )
+}
